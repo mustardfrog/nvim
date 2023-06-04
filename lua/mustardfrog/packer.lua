6fcf2a6f25e5a33703585cee838bb23029bce079
Frog <frogdoctor@tutanota.com>
2023-06-04 14:54:48 +0800

    ts autotag with treesitter

--- lua/mustardfrog/packer.lua
+++ lua/mustardfrog/packer.lua
@@ -4,104 +4,123 @@
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
-    -- themes
-    -- use '/home/chaeng/fun/scooby_blue'
-    -- use 'barrientosvctor/abyss.nvim'
-    -- use 'olimorris/onedarkpro.nvim'
+  -- themes
+  -- use '/home/chaeng/fun/scooby_blue'
+  -- use 'barrientosvctor/abyss.nvim'
+  -- use 'olimorris/onedarkpro.nvim'
 
-    use { 'rktjmp/lush.nvim' }
-    use 'luisiacc/gruvbox-baby'
-    use 'morhetz/gruvbox'
-	use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
-        -- require('rose-pine/neovim').setup({
-        --     -- dark_variant = 'main',
-        --     dark_variant = 'dawn',
-        -- }),
-		config = function()
-            -- vim.o.background='light'
-            -- vim.cmd('colorscheme rose-pine')
-		end
-	})
-    use 'folke/tokyonight.nvim'
-    -- use {
-    --     'uloco/bluloco.nvim',
-    --     requires = { 'rktjmp/lush.nvim' }
-    -- }
-    use { "catppuccin/nvim", as = "catppuccin" }
-    -- use 'cocopon/iceberg.vim'
-    use "rebelot/kanagawa.nvim"
+  use { 'rktjmp/lush.nvim' }
+  use 'luisiacc/gruvbox-baby'
+  use 'morhetz/gruvbox'
 
-    use 'norcalli/nvim-colorizer.lua'
-    use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
-    use 'nvim-treesitter/nvim-treesitter-context'
-	use('nvim-treesitter/playground')
-    use "lukas-reineke/indent-blankline.nvim"
+  use {
+    'olivercederborg/poimandres.nvim',
+    config = function()
+      require('poimandres').setup {
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      }
+    end
+  }
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    -- require('rose-pine/neovim').setup({
+    --     -- dark_variant = 'main',
+    --     dark_variant = 'dawn',
+    -- }),
+    config = function()
+      -- vim.o.background='light'
+      -- vim.cmd('colorscheme rose-pine')
+    end
+  })
+  use 'folke/tokyonight.nvim'
+  -- use {
+  --     'uloco/bluloco.nvim',
+  --     requires = { 'rktjmp/lush.nvim' }
+  -- }
+  use { "catppuccin/nvim", as = "catppuccin" }
+  -- use 'cocopon/iceberg.vim'
+  use "rebelot/kanagawa.nvim"
 
-	use('theprimeagen/harpoon')
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
-    -- use 'habamax/vim-godot'
+  use 'norcalli/nvim-colorizer.lua'
+  use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
+  use 'nvim-treesitter/nvim-treesitter-context'
+  use('nvim-treesitter/playground')
+  use "lukas-reineke/indent-blankline.nvim"
 
-    use 'simrat39/rust-tools.nvim'
-    use 'Exafunction/codeium.vim'
-    -- 'VonHeikemen/lsp-zero.nvim',
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+  -- use 'habamax/vim-godot'
 
-    -- LSP Support
-    use('neovim/nvim-lspconfig')
-    use('onsails/lspkind.nvim')
+  use 'simrat39/rust-tools.nvim'
+  use 'Exafunction/codeium.vim'
+  -- 'VonHeikemen/lsp-zero.nvim',
 
-    use('williamboman/mason.nvim')
-    use('williamboman/mason-lspconfig.nvim')
+  -- LSP Support
+  use('neovim/nvim-lspconfig')
+  use('onsails/lspkind.nvim')
 
-    -- Autocompletion
-    use('hrsh7th/nvim-cmp')
-    use('hrsh7th/cmp-buffer')
-    use('hrsh7th/cmp-path')
-    use('saadparwaiz1/cmp_luasnip')
-    use('hrsh7th/cmp-cmdline')
-    use('hrsh7th/cmp-nvim-lsp')
-    use('hrsh7th/cmp-nvim-lua')
+  use('williamboman/mason.nvim')
+  use('williamboman/mason-lspconfig.nvim')
 
-    -- Snippets
-    use('L3MON4D3/LuaSnip')
-    use('rafamadriz/friendly-snippets')
+  -- Autocompletion
+  use('hrsh7th/nvim-cmp')
+  use('hrsh7th/cmp-buffer')
+  use('hrsh7th/cmp-path')
+  use('saadparwaiz1/cmp_luasnip')
+  use('hrsh7th/cmp-cmdline')
+  use('hrsh7th/cmp-nvim-lsp')
+  use('hrsh7th/cmp-nvim-lua')
+  use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
 
-    -- Debugging
-    use 'rcarriga/nvim-dap-ui'
-    use 'mfussenegger/nvim-dap'
-    use 'theHamsta/nvim-dap-virtual-text'
-    use 'nvim-telescope/telescope-dap.nvim'
-    use 'leoluz/nvim-dap-go'
-    -- use "folke/neodev.nvim"
+  use "windwp/nvim-ts-autotag"
+  -- Snippets
+  use('L3MON4D3/LuaSnip')
+  use('rafamadriz/friendly-snippets')
 
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-    }
+  -- Debugging
+  use 'rcarriga/nvim-dap-ui'
+  use 'mfussenegger/nvim-dap'
+  use 'theHamsta/nvim-dap-virtual-text'
+  use 'nvim-telescope/telescope-dap.nvim'
+  use 'leoluz/nvim-dap-go'
+  -- use "folke/neodev.nvim"
 
-    use 'nvim-tree/nvim-web-devicons'
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional, for file icons
-        },
-        -- tag = 'nightly' -- optional, updated every week. (see issue #1193)
-    }
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+  use { "max397574/better-escape.nvim", config = function()
+    require("better_escape").setup()
+  end, }
+
+  use 'nvim-tree/nvim-web-devicons'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    -- tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
 end)
