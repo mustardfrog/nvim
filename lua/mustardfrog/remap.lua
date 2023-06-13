2b746c91b652af081d7d16b17e9ab2858e5f9abc
Frog <frogdoctor@tutanota.com>
2023-06-13 18:45:58 +0800

    better escape

--- lua/mustardfrog/remap.lua
+++ lua/mustardfrog/remap.lua
@@ -2,7 +2,6 @@ require('mustardfrog.wallpaper')
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("i", "jk", "<Esc>")
 -- vim.keymap.set({ 'n', 'v' }, "X", "<cmd>only<CR>")
 
 --vim.cmd [[
